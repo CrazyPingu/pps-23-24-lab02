4b60259aad8b25de8e57d7386b04501a50929ec9
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2024-03-14 18:10:12 +0100

    Add curriedN and nonCurriedNegr

--- src/main/scala/task2a/Neg.scala
+++ src/main/scala/task2a/Neg.scala
@@ -1,8 +1,13 @@
 package task2a
 
+// Task 2a
 def neg(predicate: String => Boolean): String => Boolean =
   (input: String) => !predicate(input)
 
-def genericNeg[X](predicate: X => Boolean): X => Boolean = 
+def genericNeg[X](predicate: X => Boolean): X => Boolean =
   (input: X) => !predicate(input)
 
+
+// Task 2b
+def curriedNeg(x: Int)(y: Int)(z: Int): Boolean = x <= y && y == z
+def nonCurriedNeg(x: Int, y: Int, z: Int): Boolean = x <= y && y == z
