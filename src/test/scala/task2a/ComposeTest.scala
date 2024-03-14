f0922bb9d1c783cd73c5af4da67da1dee722e120
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2024-03-14 18:49:22 +0100

    Create test for compose method

---
+++ src/test/scala/task2a/ComposeTest.scala
@@ -0,0 +1,19 @@
+package task2a
+
+import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Test
+
+class ComposeTest {
+
+  @Test def testCompose(): Unit = {
+    val f: Int => Int = _ - 1
+    val g: Int => Int = _ * 2
+    assertEquals(9, compose(f, g)(5))
+  }
+
+  @Test def testGenericCompose(): Unit = {
+    val f: String => String = _ + "a"
+    val g: String => String = _ * 2
+    assertEquals("testtesta", genericCompose(f, g)("test"))
+  }
+}
