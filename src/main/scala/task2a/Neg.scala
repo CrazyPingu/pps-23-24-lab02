202a6f1bb2a591189c1ba19016365a9b3ba87e38
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2024-03-14 12:52:43 +0100

    Add genericNeg

--- src/main/scala/task2a/Neg.scala
+++ src/main/scala/task2a/Neg.scala
@@ -3,4 +3,6 @@ package task2a
 def neg(predicate: String => Boolean): String => Boolean =
   (input: String) => !predicate(input)
 
+def genericNeg[X](predicate: X => Boolean): X => Boolean = 
+  (input: X) => !predicate(input)
 
