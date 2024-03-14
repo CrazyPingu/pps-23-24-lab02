cd5aa615fa76e18da4ca7f72f064873b54126424
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2024-03-14 12:37:10 +0100

    Cleaned code

--- src/test/scala/task2a/PositiveTest.scala
+++ src/test/scala/task2a/PositiveTest.scala
@@ -3,23 +3,23 @@ package task2a
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class PositiveTest :
+class PositiveTest:
 
   @Test def testPositive(): Unit = {
-    assertEquals(task2a.Sign.POSITIVE, task2a.positive(1))
+    assertEquals(Sign.POSITIVE, positive(1))
   }
 
   @Test def testNegative(): Unit = {
-    assertEquals(task2a.Sign.NEGATIVE, task2a.positive(-1))
+    assertEquals(Sign.NEGATIVE, positive(-1))
   }
 
   @Test def testZero(): Unit = {
-    assertEquals(task2a.Sign.POSITIVE, task2a.positive(0))
+    assertEquals(Sign.POSITIVE, positive(0))
   }
 
   @Test def consecutiveCalls(): Unit = {
-    assertEquals(task2a.Sign.POSITIVE, task2a.positive(1))
-    assertEquals(task2a.Sign.NEGATIVE, task2a.positive(-1))
-    assertEquals(task2a.Sign.POSITIVE, task2a.positive(0))
+    assertEquals(Sign.POSITIVE, positive(1))
+    assertEquals(Sign.NEGATIVE, positive(-1))
+    assertEquals(Sign.POSITIVE, positive(0))
   }
 
